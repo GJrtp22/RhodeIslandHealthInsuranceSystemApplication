fed56ac564a9831dd07ce94604c0c96d466a46d7
Govardhan <govardhan.sforce@gmail.com>
2023-01-30 22:11:11 +0530

    Added some constants

--- RHIS-ApplicationRegistration/src/main/java/rhis/ms/ar/constants/ApplicationRegistrationConstants.java
+++ RHIS-ApplicationRegistration/src/main/java/rhis/ms/ar/constants/ApplicationRegistrationConstants.java
@@ -2,6 +2,12 @@ package rhis.ms.ar.constants;
 
 public class ApplicationRegistrationConstants {
 	
-	public static final String SSAWEBURL="";
+	public static final String RHODEISLANDSSN="Rhode Island";
+	
+	public static final String ELIGIBLEMESSAGE="You are eligible for Rhode Island health insurance plans";
+	
+	public static final String NONELIGIBLEMESSAGE="You are not eligible, since you are not part of Rhode Island state";
+	
+	public static final String SSAWEBURL="http://ssawebapi-env.eba-k88bsahp.ap-south-1.elasticbeanstalk.com/ssn/";
 
 }
